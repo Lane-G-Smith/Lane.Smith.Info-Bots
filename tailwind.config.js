a4e22ef857d4abf963ca8d3dd8050b57d8bf2f40
Lane G Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2023-07-31 12:42:22 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js,css}"],
+  content: ["/*.{html,js,css}"],
   darkMode: "class",
   theme: {
     extend: {},
