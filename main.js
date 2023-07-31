5892618934085cc8dbe874366983ee14482ecdd9
Lane G Smith <108886772+Lane-G-Smith@users.noreply.github.com>
2023-07-31 12:22:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
+// DARK MODE
 
-// Dark Mode Toggle form https://flowbite.com/docs/customize/dark-mode/
+// attach event listener to theme toggle button / icon
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {themeSwitch()
+});
 
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+// use the prefered user theme if set
+const userTheme = localStorage.getItem('theme');
 
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
+// use the system theme if a default is set
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// function to check for current theme settings
+const themeCheck = () => {
+    if (userTheme === 'dark' || (!userTheme && systemTheme)) {
+        document.documentElement.classList.add('dark');
+        themeToggle.classList.add('display-none');
+        return;
+    }
+    themeToggle.classList.add('display-none');
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function() {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-    // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
+// manual theme toggle switch
+const themeSwitch = () => {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        return
     }
-    
-});
+    else
+    document.documentElement.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+    themeToggle();
+};
+
